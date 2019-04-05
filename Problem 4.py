73f9959c8efa6172770ecb5124da6cd6b486ea18
Mohamed Ramadan <47089306+MohamedRamadan96@users.noreply.github.com>
2019-04-05 04:47:09 +0200

    Add files via upload

---
+++ Problem 4.py
@@ -0,0 +1,7 @@
+# Question 4 : Given a string and an int n,
+# remove characters from string starting from zero upto n and return a new string
+
+def remover_Chars(str,num):
+    return str[num:]
+
+print(remover_Chars('Mohamed',4))
